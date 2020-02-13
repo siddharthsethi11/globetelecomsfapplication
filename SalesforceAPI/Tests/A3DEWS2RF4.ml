e8506540b6a4bf915fa5cffefb786c5b981cb33d
sisethi <sisethi@tibco.com>
2020-02-13 08:04:15 -0500

    commit unit test v1

---
+++ SalesforceAPI/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/SalesforceAPI/Tests/SalesforceAPI1.bwt#/"/>
+  <EmulationDataFile>ACED000574001253616C6573666F726365415049312E627774</EmulationDataFile>
+</emulation:BWTFile>
